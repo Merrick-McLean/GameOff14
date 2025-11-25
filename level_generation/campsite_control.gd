39d0e2fac66c2c15d9d1b64bb74ce5665e0610c3
Jett <92655142+Cthulhu5227@users.noreply.github.com>
2025-11-24 16:55:17 -0800

    functional campers, need some work but spawn despawn and light now

--- level_generation/campsite_control.gd
+++ level_generation/campsite_control.gd
@@ -18,9 +18,21 @@ func _on_tick():
 	var n = randf()
 	if n < 0.001*campers.size():
 		light_tree()
+	elif n > 0.999*1/campers.size():
+		despawn_camper()
 	elif n < 0.01 and campers.size() < max_campers:
 		spawn_camper()
-		
+
+func despawn_camper():
+	var tribute = campers.pick_random()  #hunger games refrence
+	
+	if tribute.lighter:
+		return
+	
+	tribute.hide()
+	campers.erase(tribute)
+	tribute.queue_free()
+	
 func spawn_camper():
 	var camper = camper_scene.instantiate()
 	camper.z_index = global_position.y
