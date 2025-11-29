d03c2b907a675d5931f66f2fc6ae8b1e36de3a55
Jett <92655142+Cthulhu5227@users.noreply.github.com>
2025-11-29 15:31:35 -0800

    ui sizing issues, and connecting summer signals

--- UI/economy.gd
+++ UI/economy.gd
@@ -1,1 +1,9 @@
 extends Node
+
+func _ready(): 
+	var weather = get_tree().get_current_scene().get_node("Level/weather_control")
+	weather.new_summer.connect(_new_summer)
+	
+
+func _new_summer():
+	pass
