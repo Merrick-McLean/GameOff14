73c3e208c378b40b00a6e9e565d033d832ba0896
Merrick McLean <merrickmmclean@gmail.com>
2025-11-30 23:00:42 -0800

    adds fire animation

--- level_generation/level_generate.gd
+++ level_generation/level_generate.gd
@@ -357,7 +357,8 @@ func spawn_tree(min_x, max_x, min_y, max_y):
 	tree.seed = idxes[0]
 	tree.set_texture(idxes[0]) #main idx
 	add_child(tree)
-	tree.modulate = get_color_for_index(idxes[0])
+	var tree_sprite = tree.get_node("Sprite2D")
+	tree_sprite.modulate = get_color_for_index(idxes[0])
 	set_camptree(tree)
 
 func get_flow_direction(pos: Vector2) -> Vector2:
