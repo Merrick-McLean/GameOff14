6319b91a87b147032b1acad261688654c392af92
JohnnyTheBus <willytcalder@gmail.com>
2025-12-23 18:31:11 -0800

    Fixed a few issues with the settings panel

--- UI/settings_button.gd
+++ UI/settings_button.gd
@@ -3,19 +3,19 @@ extends Button
 func _ready():
 	pressed.connect(_on_button_pressed)
 	tooltip_text = "Settings"
-	$ReturnToGame.visible = false
-	$QuitToMenu.visible = false
-	$QuitGame.visible = false
+	get_node("VBoxContainer/ReturnToGame").visible = false
+	get_node("VBoxContainer/QuitToMenu").visible = false
+	get_node("VBoxContainer/QuitGame").visible = false
 
 func _on_button_pressed():
-	$ReturnToGame.visible = !$ReturnToGame.visible
-	$QuitToMenu.visible = !$QuitToMenu.visible
-	$QuitGame.visible = !$QuitGame.visible
+	get_node("VBoxContainer/ReturnToGame").visible = !get_node("VBoxContainer/ReturnToGame").visible
+	get_node("VBoxContainer/QuitToMenu").visible = !get_node("VBoxContainer/QuitToMenu").visible
+	get_node("VBoxContainer/QuitGame").visible = !get_node("VBoxContainer/QuitGame").visible
 
 func _on_return_to_game_pressed() -> void:
-	$ReturnToGame.visible = false
-	$QuitToMenu.visible = false
-	$QuitGame.visible = false
+	get_node("VBoxContainer/ReturnToGame").visible = false
+	get_node("VBoxContainer/QuitToMenu").visible = false
+	get_node("VBoxContainer/QuitGame").visible = false
 
 func _on_quit_to_menu_pressed() -> void:
 	get_tree().change_scene_to_file("res://main_menu.tscn")
