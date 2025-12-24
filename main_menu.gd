f6e6c19f2372e67db84473a9a6ec03a9177db9dc
JohnnyTheBus <willytcalder@gmail.com>
2025-12-23 16:43:18 -0800

    Added game main menu

---
+++ main_menu.gd
@@ -0,0 +1,9 @@
+extends Control
+
+func _on_play_game_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://loading_screen.tscn")
+
+
+func _on_quit_game_button_pressed() -> void:
+	get_tree().quit()
+	
